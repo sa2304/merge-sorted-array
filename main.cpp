f3dc29d5968f0027516ef1fc82be6a28151f44b8
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-27 17:28:00 +0700

    Solution: temp copy first array

--- main.cpp
+++ main.cpp
@@ -6,8 +6,25 @@ using namespace std;
 
 class Solution {
  public:
-  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
-    // FIXME
+  void merge(vector<int> &a, int m, vector<int> &b, int n) {
+    vector<int> a_data(m);
+    uninitialized_move_n(a.begin(), m, a_data.begin());
+    auto ap = a_data.begin();
+    auto bp = b.begin();
+    auto pout = a.begin();
+    while (a_data.end() != ap && b.end() != bp) {
+      if (*ap < *bp) {
+        *pout++ = *ap++;
+      } else {
+        *pout++ = *bp++;
+      }
+    }
+    while (a_data.end() != ap) {
+      *pout++ = *ap++;
+    }
+    while (b.end() != bp) {
+      *pout++ = *bp++;
+    }
   }
 };
 
